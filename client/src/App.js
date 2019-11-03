fbffec3c39842b4c9338b61d509b58b03e832289
Jonny Edwards <jonathan44472@gmail.com>
2019-11-03 16:03:35 +0000

    Logo added to page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <h1>LMAOOOO</h1>;
+  return (
+    <div className="container">
+      <img src="https://bit.ly/36rpQo2" alt="welcome art" />
+    </div>
+  );
 };
 
 export default App;
