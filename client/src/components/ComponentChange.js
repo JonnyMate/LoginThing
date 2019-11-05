ba26aba65bc7ceb96000a0ae19ebf4ea07149c55
Jonny Edwards <jonathan44472@gmail.com>
2019-11-05 19:57:33 +0000

    Updated ComponentChange to display correct info

--- client/src/components/ComponentChange.js
+++ client/src/components/ComponentChange.js
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 const ComponentChange = props => {
   return (
     <div className="input-switch">
-      <p>Don't have an account?</p>
+      <p>
+        {props.data ? "Don't have an account?" : "Already have an account?"}
+      </p>
       <Link to={props.data ? "/signup" : "/login"}>
         <p style={{ textDecoration: "underline", cursor: "pointer" }}>
-          Register
+          {props.data ? "Register" : "Login"}
         </p>
       </Link>
     </div>
